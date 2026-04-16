8a01b123c0af462a025ec623c5f1e0f4f332afe7
gengenweb <caldwellengen@gmail.com>
2026-04-16 17:26:18 -0500

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,26 @@
+const CACHE = 'gengen-v2';
+const ASSETS = [
+  '/gengen/',
+  '/gengen/index.html',
+  '/gengen/manifest.json',
+  '/gengen/icon-192.png',
+  '/gengen/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
